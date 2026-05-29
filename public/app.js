62dac8e136b55d46de99c2441f0e492e62120751
Elan Hickler <elanhickler@gmail.com>
2026-05-28 20:24:40 -0700

    Move module title above actions

--- public/app.js
+++ public/app.js
@@ -9011,6 +9011,16 @@ function createNodeGraphModuleElement(type, node) {
 
   const header = document.createElement("div");
   header.className = "dsp-node-header";
+  const titleRow = document.createElement("div");
+  titleRow.className = "node-header-title-row";
+  const titleText = document.createElement("span");
+  titleText.className = "node-header-title";
+  titleText.textContent = node === type
+    ? nodeGraphNodeLabels[type]
+    : `${nodeGraphNodeLabels[type]} ${node.split("-").at(-1)}`;
+  titleRow.append(titleText);
+  header.append(titleRow);
+
   const actionRow = document.createElement("div");
   actionRow.className = "node-header-actions";
   const handle = document.createElement("button");
@@ -9047,16 +9057,6 @@ function createNodeGraphModuleElement(type, node) {
   actionButton.textContent = "⚙";
   actionRow.append(actionButton);
   header.append(actionRow);
-
-  const titleRow = document.createElement("div");
-  titleRow.className = "node-header-title-row";
-  const titleText = document.createElement("span");
-  titleText.className = "node-header-title";
-  titleText.textContent = node === type
-    ? nodeGraphNodeLabels[type]
-    : `${nodeGraphNodeLabels[type]} ${node.split("-").at(-1)}`;
-  titleRow.append(titleText);
-  header.append(titleRow);
 
   article.append(header);
 
